787c290460a60594357d6614fda0b2a52313fc97
Your NameDenis <tdv14011984@gmail.com>
2022-06-20 15:39:10 +0300

    add tests

--- src/test/java/denis/timushev/ReqresinTests.java
+++ src/test/java/denis/timushev/ReqresinTests.java
@@ -2,6 +2,8 @@ package denis.timushev;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.core.Is.is;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 
@@ -21,5 +23,40 @@ public class ReqresinTests {
         get("https://reqres.in/api/users/23")
                 .then()
                 .statusCode(404);
+    }
+
+    @Test
+    void singleTest() {
+        given()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"));
+    }
+
+    @Test
+    void singleNotFoundTest() {
+        given()
+                .when()
+                .get("https://reqres.in/api/unknown/23")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    void unsuccessfulTest() {
+
+        String data = "{ \"email\": \"sydney@fife\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
     }
 }
